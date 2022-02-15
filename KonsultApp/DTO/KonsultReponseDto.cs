a95a6d278ee2acd4d78db35ee8aa0db419643d89
Heybe Feisal <46941322+heybefeisal@users.noreply.github.com>
2022-02-15 16:25:30 +0100

    DTO for konsult

---
+++ KonsultApp/DTO/KonsultReponseDto.cs
@@ -0,0 +1,18 @@
+﻿using KonsultApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonsultApp.DTO
+{
+    public class KonsultReponseDto
+    {
+        public int KonsultId { get; set; }
+        public string ForNamn { get; set; }
+        public string EfterNamn { get; set; }
+        public int AntalAr { get; set; }
+
+        public virtual ICollection<Bonu> Bonus { get; set; }
+    }
+}
